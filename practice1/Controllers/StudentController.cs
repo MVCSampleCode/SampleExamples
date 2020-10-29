3c452819948d02d8c45d78be84b8c67ff5a2f554
Dollina <mendonca.dollina@gmail.com>
2020-10-29 22:47:36 +0530

    html helper sample code

---
+++ practice1/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using practice1.Models;
+namespace practice1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [Route("DisplayStudent")]
+        public ActionResult Index()
+        {
+            Student stud = new Student();
+            return View();
+        }
+    }
+}
---
+++ practice1/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace practice1.Models
+{
+    public class Student
+    {
+        public int StudID { get; set; }
+
+        [DisplayName("Student Name")]
+        public string StudName { get; set; }
+
+        [DisplayName("Newly Enrolled")]
+        public bool isNewlyEnrolled { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
